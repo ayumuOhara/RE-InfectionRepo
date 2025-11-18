bdeafc618b5d526c6b98c7d204b2c27ad713dcc0
2459338 <kmm2459338@stu.o-hara.ac.jp>
2025-11-18 12:40:49 +0900

    編成画面
    UIちょっと変えてみました。また後日修正します

--- Re-Infection/Assets/Scripts/SceneChangeButton.cs
+++ Re-Infection/Assets/Scripts/SceneChangeButton.cs
@@ -5,15 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeButton:MonoBehaviour
 {
+    public GameObject SceneInChange;
+    public GameObject Loading_text;
     //ï“ê¨
-    public void InputSelectionButton()
+    private void Start()
     {
+        SceneInChange.SetActive(false);
+        Loading_text.SetActive(false);
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(SceneInChange);
+        DontDestroyOnLoad(Loading_text);
+    }
+    public void InputActionButton()
+    {
+        StartCoroutine(InputActionScene());
+    }
+    public void InputSceneButton()
+    {
+        
         SceneManager.LoadScene("SelectionScene");
     }
 
     //êÌì¨
-    public void InputActionButton()
+    public IEnumerator InputActionScene()
     {
+        SceneInChange.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        Loading_text.SetActive(true);
+        yield return new WaitForSeconds(3.0f);
         SceneManager.LoadScene("MainScene");
     }
 
