24c6d71aeec0c9128ca589986238834c7085c23a
kmm2459344 <kmm2459344@stu.o-hara.ac.jp>
2026-01-27 16:07:25 +0900

    【機能追加】ステージ選択できるようになった
    ステージ選択が可能になりました

--- Re-Infection/Assets/ScriptableObjects/Scripts/StageData.cs
+++ Re-Infection/Assets/ScriptableObjects/Scripts/StageData.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "StageData", menuName = "Scriptable Objects/StageData")]
 public class StageData : ScriptableObject
 {
+    public Stage[] Stage;
     public bool[] isStageClear;//クリア状況の記録
     public bool[] isStageOpen;//解放済みかのフラグ
 
-
+    public int SelectStageNumber;
 }
